494add554f33682d1349e5a7da0b177bea76ffdb
Akbar Hamonangan Lubis <alubis87@gmail.com>
2026-02-21 14:30:54 +0700

    feat: add build:server script and update Vite configuration for server builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    proxy: {
-      '/aplicares': {
-        target: 'https://faskes.bpjs-kesehatan.go.id',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(() => {
+  // Pakai cara ini biar lebih aman di lingkungan Node/Vite
+  const isServer = process.env.BUILD_TARGET === 'server';
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    build: {
+      // Pastiin folder /var/www/cek-kamar sudah lo 'chown' ke user ubuntu ya!
+      outDir: isServer ? '/var/www/cek-kamar' : 'dist',
+      emptyOutDir: true,
+    },
+    server: {
+      proxy: {
+        '/aplicares': {
+          target: 'https://faskes.bpjs-kesehatan.go.id',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
